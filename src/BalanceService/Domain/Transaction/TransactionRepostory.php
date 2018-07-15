9dee362c73670090e53984b66eea0ce7babcc289
buckmulligan <malachibuckmulligan@gmail.com>
2018-07-15 15:17:25 +0300

    fixed code style

--- src/BalanceService/Domain/Transaction/TransactionRepostory.php
+++ src/BalanceService/Domain/Transaction/TransactionRepostory.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Iqoption\BalanceService\Domain\Transaction;
 
-use Iqoption\BalanceService\Domain\Transaction\Transaction;
-
 interface TransactionRepostory
 {
     public function add(Transaction $transaction): void;
