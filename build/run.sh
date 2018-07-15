195439153032cc685c6c1591ca1d35bc107bed4b
buckmulligan <malachibuckmulligan@gmail.com>
2018-07-15 21:56:27 +0300

    forced fail if composer install fails

--- build/run.sh
+++ build/run.sh
@@ -11,6 +11,6 @@ fi
 cd $DIR
 
 docker-compose --project-name balance-service build && \
-docker-compose --project-name balance-service up balance-service-composer
+docker-compose --project-name balance-service up balance-service-composer && \
 docker-compose --project-name balance-service up --scale balance-service-worker=3 balance-service-worker balance-service-rabbitmq balance-service-postgres
 
