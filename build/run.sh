3c4a12a2be487c8f9f86e6643e413458dc245c6b
buckmulligan <malachibuckmulligan@gmail.com>
2018-07-15 21:53:25 +0300

    forced rabbitmq and postgres stop

--- build/run.sh
+++ build/run.sh
@@ -12,5 +12,5 @@ cd $DIR
 
 docker-compose --project-name balance-service build && \
 docker-compose --project-name balance-service up balance-service-composer
-docker-compose --project-name balance-service up --scale balance-service-worker=3 balance-service-worker
+docker-compose --project-name balance-service up --scale balance-service-worker=3 balance-service-worker balance-service-rabbitmq balance-service-postgres
 
